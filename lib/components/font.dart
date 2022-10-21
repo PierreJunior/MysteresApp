9145228724b15a117de13cabc75395dea413eba1
Gloire Kahozi <gloire.kahozi@dimensiondata.com>
2022-10-21 16:37:55 +0200

    Update heading font sizes.

--- lib/components/font.dart
+++ lib/components/font.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class Font {
   static const heading1 = TextStyle(
-    fontSize: 30,
+    fontSize: 40,
     color: Colors.black,
     fontWeight: FontWeight.w600,
   );
 
-  static const heading3 = TextStyle(fontSize: 30, color: Colors.white70);
+  static const heading3 = TextStyle(fontSize: 25, color: Colors.white70);
 
   static const paragraph = TextStyle(fontSize: 20, color: Colors.white70);
 
