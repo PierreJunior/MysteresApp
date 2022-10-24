25d05b053ca99076016a2d3ab83bf1913cf563c9
PierreJunior <pierresangwa@gmail.com>
2022-10-24 13:44:57 +0200

    added ads container, progress bar and changed position of the Buttons and the title

---
+++ lib/components/ads.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'color_palette.dart';
+
+class Ads extends StatelessWidget {
+  const Ads({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width,
+      color: ColorPalette.primaryDark,
+      child: const Center(child: Text('Ads go here')),
+    );
+  }
+}
