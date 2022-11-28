7dc155978ba7c2d3f32039eadd8fc27bb0b45a60
Gloire Kahozi <gloire.kahozi@dimensiondata.com>
2022-11-28 20:30:44 +0200

    Install and configure sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:mysteres/ads_state.dart';
+import 'package:mysteres/env.dart';
 import 'package:provider/provider.dart';
 import 'package:mysteres/screens/splash_screen.dart';
 import 'package:mysteres/screens/landing_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sentry_logging/sentry_logging.dart';
 import 'firebase_options.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final initFuture = MobileAds.instance.initialize();
   final adState = AdState(initFuture);
-  runApp(
-    Provider.value(
-      value: adState,
-      builder: (context, child) => const MyApp(),
-    ),
-  );
+  await SentryFlutter.init((options) {
+    options.dsn = Env.sentryDSN;
+    options.tracesSampleRate = double.parse(Env.sentryTraceSampleRate);
+    options.addIntegration(LoggingIntegration());
+  }, appRunner: () {
+    runApp(
+      Provider.value(
+        value: adState,
+        builder: (context, child) => const MyApp(),
+      ),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
