1e53c414155816a70f48ae2b6e189db6fe44a38c
Gloire Kahozi <gloire.kahozi@dimensiondata.com>
2023-01-11 15:58:32 +0200

    Remove unused imports.

--- test/services/week_days_service_test.dart
+++ test/services/week_days_service_test.dart
@@ -2,11 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-import 'package:mysteres/services/language_service.dart';
 import 'package:mysteres/services/week_days_service.dart';
 
 import '../mock.dart';
-import '../test_helper.dart';
 
 void main() {
   setupFirebaseAuthMocks();
